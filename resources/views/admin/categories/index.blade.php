b9fa7fafd2fc775f499488cc2d62329bb936c9f7
utsab <shakhawatutsab@gmail.com>
2025-03-25 14:59:13 +0600

    Remove category with popup

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -75,7 +75,32 @@
                           <form method="POST"action="{{route('categories.destroy',$category->id)}} ">
                               @csrf
                               @method('DELETE')
-                              <button class="btn btn-danger" type="submit">Delete</button>
+                              <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                                Delete
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Remove Category: {{ $category->title }}</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    </div>
+                                    <div class="modal-body">
+                                    Are you sure you remove the category permanently
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+
                           </form>
                       </td>
                     </tr>
